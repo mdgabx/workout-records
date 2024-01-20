bba75471a92dcd97335436d1fa439eabca9be61a
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-01-20 21:21:34 +0800

    add: Model and schemas

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -15,8 +16,23 @@ router.get('/:id', (req, res) => {
 
 
 // Create a new workout
-router.post('/', (req, res) => {
-    res.json({ msg: 'POST a new workouts' })
+router.post('/', async (req, res) => {
+    const { title, load, reps } = req.body
+    
+    try {
+        const workout = await Workout.create({
+            title,
+            load,
+            reps
+        })
+
+        res.status(200).json(workout)
+
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+
+    // res.json({ msg: 'POST a new workouts' })
 })
 
 
