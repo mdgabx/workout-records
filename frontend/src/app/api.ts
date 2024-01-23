57e99251e737fad00c23134057b276fa39cbd1db
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-01-23 17:45:43 +0800

    add: styles

--- frontend/src/app/api.ts
+++ frontend/src/app/api.ts
@@ -6,4 +6,9 @@ export const API_BASE_URL = 'https://workout-crud.onrender.com/api' // url
 export const fetchWorkouts = async () => {
     const response = await axios.get(`${API_BASE_URL}/workouts/`);
     return response.data;
-  };
+};
+
+export const createWorkout = async () => {
+    
+}
+
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -18,15 +18,20 @@ const Home = () => {
   }, [workouts]);
 
   return (
-    <div>
+    <div className="font-urbanist flex flex-col items-center justify-evenly">
+      <h2 className="font-urbanist text-left">Records</h2>
+      
+      <div className="mx-auto w-100 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 my-10">
       {workouts.map((workout: Workout) => (
-        <div key={workout._id}>
+        <div key={workout._id} className="shadow-md border border-gray-200 p-7 rounded">
           <h2>{workout.title}</h2>
           <p>Reps: {workout.reps}</p>
           <p>Load: {workout.load}</p>
         </div>
       ))}
     </div>
+    </div>
+    
   );
 };
 
